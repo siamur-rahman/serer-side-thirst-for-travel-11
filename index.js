35876d7ab5c0c40a766e367cad85673e24c8c0eb
unknown <siamur01@gmail.com>
2021-10-31 02:42:04 +0600

    commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-const port = 5000;
+const port = process.env || 5000;
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wrybq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
